f6a866e6346e7dd2c8dc0aae944bf81f99a4edc7
Angelo James Arriola <angelojames.arriola@gmail.com>
2018-11-20 12:24:33 -0800

    created delete route for users endpoint

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -5,16 +5,26 @@ const Users = require('../models/user.models');
 
 router.get('/', (req, res) => {
   Users.find()
-  .then(users => res.json(users))
+  .then(users => res.status(200).json({ data: users }))
 });
 
 router.post('/', (req, res) => {
+  let { username, password, email } = req.body;
   const newUser = new Users({
-    name: req.body.username,
-    password: req.body.password,
-    email: req.body.email
+    name: username,
+    password: password,
+    email: email
   })
-  newUser.save().then(users => res.json(users))
-})
+  newUser.save().then(users => res.status(200).json({ data: users, message: 'Successfully created a new user.' }))
+    .catch(error => res.status(404).json({ data: error, message: 'Failed to create a user.' }))
+});
+
+router.delete('/:id', (req, res) => {
+  const userId = req.params.id
+
+  Users.findById(userId)
+    .then(item => item.remove().then(() => res.status(200).json({ message: 'Successfuly deleted user account.' })))
+    .catch(error => res.status(404).json({ message: 'Failed to delete user.' }))
+});
 
 module.exports = router;
