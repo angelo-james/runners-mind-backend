74fa85bd8143d235aed6ef218686ee10f2064810
Angelo James Arriola <angelojames.arriola@gmail.com>
2018-11-20 12:22:58 -0800

    added proccess.env.PORT to the port variable for later use with heroku

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3800;
+const port = process.env.PORT || 3800;
 
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
