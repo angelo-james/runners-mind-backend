1c69d5c730f8b4c38a74f7f4bca360ac86393816
Angelo James Arriola <angelojames.arriola@gmail.com>
2018-11-30 09:37:43 -0800

    created endpoint for user login

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -9,6 +9,7 @@ router.get('/', (req, res) => {
 });
 
 router.post('/', (req, res) => {
+  console.log(req.body);
   let { username, password, email } = req.body;
   const newUser = new Users({
     username: username,
@@ -17,6 +18,17 @@ router.post('/', (req, res) => {
   })
   newUser.save().then(users => res.status(200).json({ data: users, message: 'Successfully created a new user.' }))
     .catch(error => res.status(404).json({ data: error, message: 'Failed to create a user.' }))
+});
+
+router.post('/login', async (req, res) => {
+  let { email, password } = req.body;
+  let user = await Users.find({email: email})
+  return res.status(200).json({
+    username: user[0].username,
+    followers: user[0].followers,
+    following: user[0].following,
+    userId: user[0]._id
+  })
 });
 
 router.delete('/:id', (req, res) => {
